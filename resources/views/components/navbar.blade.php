f9d02f44e10ee5451dba2a0ed58d4505934ef827
pius11 <pius17431@gmail.com>
2024-12-21 22:09:40 +0800

    fix bug search in navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -53,7 +53,7 @@
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-4.35-4.35M17 10A7 7 0 1 1 3 10a7 7 0 0 1 14 0z" />
             </svg>
           </button>
-          <input type="text" name="q" placeholder="Cari..." class="pl-10 pr-4 py-2 w-64 border rounded-3xl focus:outline-none">
+          <input type="text" name="query" placeholder="Cari..." class="pl-10 pr-4 py-2 w-64 border rounded-3xl focus:outline-none">
         </form>
 
         <!-- Cart Icon -->
